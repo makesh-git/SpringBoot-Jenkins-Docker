708eced14ac53e0d2e3fed6db8815fec357b17e2
Makesh <mynameismakesh@gmail.com>
2020-07-21 12:59:58 +0530

    AAFFEED

--- src/main/java/com/main/SpringBootJenkinsApplication.java
+++ src/main/java/com/main/SpringBootJenkinsApplication.java
@@ -14,7 +14,7 @@ public class SpringBootJenkinsApplication {
 	
 	@PostConstruct
 	public void init() {
-		logger.info("Application Started ...");
+		logger.info("Application ..Started ...");
 	}
 	
 	public static void main(String[] args) {
